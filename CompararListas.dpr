056970b12769f582aec0aed53c6493645c7ba5b4
Jeihcio Francis <jeihcio.francis@hotmail.com>
2020-07-28 21:13:20 -0300

    Adicinando o verificador de memory leak

--- CompararListas.dpr
+++ CompararListas.dpr
@@ -10,6 +10,8 @@ uses
 {$R *.res}
 
 begin
+  ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFrmPrincipal, FrmPrincipal);
