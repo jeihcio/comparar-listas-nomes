14119bbac471ae617d4fbe4b2f7f2acfbd4c3139
Jeihcio Francis <jeihcio.francis@hotmail.com>
2020-07-30 19:39:40 -0300

    Update UntOpListas.pas

--- controller/public/UntOpListas.pas
+++ controller/public/UntOpListas.pas
@@ -3,22 +3,47 @@ unit UntOpListas;
 interface
 
 uses
-   System.Classes;
+   System.Classes, UntCompararDuasListaResult, Vcl.ComCtrls, UntBarraProgresso,
+   UntOpStrings;
 
 type
    TOpLista = class
+   private
+      FBarraProgresso: TBarraProgresso;
+      FStrings: TOpStrings;
    public
+      constructor Create(); reintroduce;
+      destructor Destroy(); override;
+
       function getListaComItensSemEspacosEMaiusculo(ALista: TStrings)
         : TStringList;
 
       function localizarItemLista(AListaComItensSemEspacoEMaiusculos
-        : TStringList; AValue: String): Boolean;
+        : TStrings; AValue: String): Boolean;
+
+      function verificarSeItensDeUmaListaContemEmOutra
+        (AProgressBar: TProgressBar;
+        AListaParcial, AListaCompletaSemEspacoEMaiusculo: TStrings)
+        : TCompararDuasListaResult;
    end;
 
 implementation
 
 uses
    System.SysUtils;
+
+constructor TOpLista.Create;
+begin
+   FBarraProgresso := TBarraProgresso.Create();
+   FStrings := TOpStrings.Create();
+end;
+
+destructor TOpLista.Destroy;
+begin
+   FBarraProgresso.Free;
+   FStrings.Free;
+   inherited;
+end;
 
 function TOpLista.getListaComItensSemEspacosEMaiusculo(ALista: TStrings)
   : TStringList;
@@ -35,10 +60,34 @@ begin
 end;
 
 function TOpLista.localizarItemLista(AListaComItensSemEspacoEMaiusculos
-  : TStringList; AValue: String): Boolean;
+  : TStrings; AValue: String): Boolean;
 begin
    Result := (AListaComItensSemEspacoEMaiusculos.IndexOf
      (UpperCase(AValue)) > -1);
+end;
+
+function TOpLista.verificarSeItensDeUmaListaContemEmOutra
+  (AProgressBar: TProgressBar; AListaParcial, AListaCompletaSemEspacoEMaiusculo
+  : TStrings): TCompararDuasListaResult;
+var
+   indexListaParcial: Integer;
+   nomeListaParcial: String;
+begin
+   Result := TCompararDuasListaResult.Create();
+
+   for indexListaParcial := 0 to AListaParcial.Count - 1 do
+   begin
+      nomeListaParcial := FStrings.removerEspacos
+        (AListaParcial[indexListaParcial]);
+
+      if localizarItemLista(AListaCompletaSemEspacoEMaiusculo, nomeListaParcial)
+      then
+         Result.ListaNomesContemListaCompleta.Add(nomeListaParcial)
+      else
+         Result.ListaNomesNaoContemListaCompleta.Add(nomeListaParcial);
+
+      FBarraProgresso.incBarraProgresso(AProgressBar);
+   end;
 end;
 
 end.
